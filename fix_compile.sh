2c819c87bf9875eeb4e8119410715dbcc3e4b2e5
zhangsaijun <zsj578027398@gmail.com>
2026-07-17 14:15:01 +0800

    padding 更适合

---
+++ fix_compile.sh
@@ -0,0 +1,4 @@
+cp /usr/local/lib/python3.12/dist-packages/nvidia/cu13/include/cusolver*.h /usr/local/cuda-13.0/include/
+cp /usr/local/lib/python3.12/dist-packages/nvidia/cu13/include/cusparse*.h /usr/local/cuda-13.0/include/
+cp /usr/local/lib/python3.12/dist-packages/nvidia/cu13/include/nvrtc.h /usr/local/cuda-13.0/include/
+
